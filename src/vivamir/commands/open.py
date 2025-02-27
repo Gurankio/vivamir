1b77cd66d65f9c18a0ab6381c785fc207b95bae9
Jacopo Del Granchio <jacopo@delgranchio.it>
2025-02-27 12:10:55 +0100

    Colored logs, auto waveform loading, fixed export command.

--- src/vivamir/commands/open.py
+++ src/vivamir/commands/open.py
@@ -1,27 +1,77 @@
+import contextlib
+import dataclasses
+import functools
+import pty
+import re
 import subprocess
-from time import sleep
-from typing import Iterator
+from typing import Optional
 
-import sys
 from rich import print
+from rich.console import Console
 
 from vivamir.commands.generate import command_generate
 from vivamir.vivamir import Vivamir
 
 
-def _follow(file, sleep_sec=0.125) -> Iterator[str]:
-    """ Yield each line from a file as they are written.
-    `sleep_sec` is the time to sleep after empty reads. """
-    line = ''
-    while True:
-        tmp = file.readline()
-        if tmp is not None and tmp != "":
-            line += tmp
-            while '\n' in line:
-                out, line = line.split('\n', 1)
-                yield out
-        elif sleep_sec:
-            sleep(sleep_sec)
+@dataclasses.dataclass()
+class TaskReport:
+    task: str
+    cpu: str
+    elapsed: str
+    peak: str
+    gain: str
+    free_physical: str
+    free_virtual: str
+
+    @staticmethod
+    @functools.cache
+    def pattern():
+        return re.compile(
+            r'^(.+): Time \(s\): cpu = (.+?); elapsed = (.+?) \. Memory \(MB\): peak = (.+?) ; gain = (.+?) ; free physical = (.+?) ; free virtual = (.+?)$'
+        )
+
+    @classmethod
+    def parse_if_match(cls, line: str) -> Optional['TaskReport']:
+        if match := cls.pattern().match(line):
+            return TaskReport(*match.groups())
+
+    def __str__(self):
+        return f'INFO: [vivamir::report] {self.task} in {self.elapsed}s'
+
+
+@contextlib.contextmanager
+def clean_vivado(vivamir: Vivamir, vivado_executable: list[str]):
+    process = None
+    try:
+        _, fake_stdin = pty.openpty()
+        process = subprocess.Popen(
+            args=[
+                *vivado_executable, '-mode', 'batch', '-source', 'open.tcl'
+            ],
+            cwd=vivamir.root / 'vivamir',
+            stdin=fake_stdin,
+            text=True,
+            universal_newlines=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
+        print('INFO: [Vivamir] Vivado started.')
+        yield process
+    finally:
+        if process is not None:
+            try:
+                print('\nINFO: [Vivamir] Awaiting Vivado termination.')
+                stdout, stderr = process.communicate(timeout=15)
+                if stdout is not None:
+                    print(stdout)
+                if stderr is not None:
+                    print(stderr)
+
+            except (KeyboardInterrupt, subprocess.TimeoutExpired):
+                process.kill()
+                print('INFO: [Vivamir] Vivado terminated forcefully.')
+            else:
+                print('INFO: [Vivamir] Vivado terminated.')
 
 
 def command_open(vivado_executable: list[str]):
@@ -35,57 +85,40 @@ def command_open(vivado_executable: list[str]):
     print('INFO: [Vivamir] Regenerating scripts...')
     command_generate()
 
-    process = None
-    tail = None
+    console = Console()
+    buffer = ''
+    style = ''
 
-    try:
-        logfile = vivamir.root / 'vivamir' / 'vivado.log'
-        logfile.unlink(missing_ok=True)
-        # logfile.touch()
-
-        process = subprocess.Popen(
-            args=[
-                *vivado_executable, '-mode', 'batch', '-source', 'open.tcl'
-            ],
-            cwd=vivamir.root / 'vivamir',
-            text=True,
-            # universal_newlines=True,
-            # bufsize=16,
-            # stdin=subprocess.DEVNULL,
-            stdout=subprocess.DEVNULL,
-            stderr=subprocess.DEVNULL,
-            # stdin=sys.stdin,
-        )
-
-        print('INFO: [Vivamir] Vivado started.')
-
-        while process.poll() and not logfile.exists():
-            sleep(0.100)
-
-        if process.poll():
-            tail = subprocess.Popen(
-                args=['tail', '-n50', '-f', str(logfile.resolve())],
-                stdout=sys.stdout,
-                stderr=sys.stderr,
-            )
-
+    with clean_vivado(vivamir, vivado_executable) as process:
         while process.poll() is None:
-            sleep(0.250)
-
-    finally:
-        if tail is not None:
-            tail.kill()
-
-        if process is not None:
             try:
-                print('INFO: [Vivamir] Awaiting Vivado termination.')
-                out, err = process.communicate(timeout=30)
-                if out is not None:
-                    print(out)
-                if err is not None:
-                    print(err)
-            except (KeyboardInterrupt, subprocess.TimeoutExpired):
-                process.kill()
-                print('INFO: [Vivamir] Vivado terminated forcefully.')
-            else:
-                print('INFO: [Vivamir] Vivado terminated.')
+                line = process.stdout.readline().rstrip()
+
+                if task := TaskReport.parse_if_match(line):
+                    console.print(str(task), style='dim')
+                    continue
+
+                if line.startswith('##'):
+                    continue
+
+                line = line.removeprefix('# ')
+                buffer += line
+
+                if line.startswith('INFO: '):
+                    style = 'dim'
+
+                if line.startswith('WARNING: '):
+                    style = 'yellow'
+
+                if line.startswith('CRITICAL WARNING: ') or line.startswith('ERROR: '):
+                    style = 'bold red'
+
+                if line.endswith(':'):
+                    continue
+
+                console.print(line, style=style, markup=False, highlight=False)
+                buffer = ''
+                style = ''
+
+            except Exception as e:
+                print(f'ERROR: Python crashed with {e!s}')
